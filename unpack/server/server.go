c46f752bff932c1ddad4a1665a3c1c11aec0fee9
pengzhe <pengzhepost@sina.com>
2021-09-04 13:51:25 +0800

    unpack

---
+++ unpack/server/server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"Learning-Go-Web/unpack"
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+)
+
+// socket_stick/server2/main.go
+
+func process(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := unpack.Decode(reader)
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			fmt.Println("decode msg failed, err:", err)
+			return
+		}
+		fmt.Println("收到client发来的数据：", msg)
+	}
+}
+
+func main() {
+
+	listen, err := net.Listen("tcp", "127.0.0.1:30000")
+	if err != nil {
+		fmt.Println("listen failed, err:", err)
+		return
+	}
+	defer listen.Close()
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("accept failed, err:", err)
+			continue
+		}
+		go process(conn)
+	}
+}
